115db4c15d7ffe6870d88220917e31394c2a761a
boughtonjennifer <boughtonjennifer@uchicago.edu>
2018-11-13 21:51:44 -0600

    all done

--- buffer_by_centroids.R
+++ buffer_by_centroids.R
@@ -5,6 +5,7 @@ library(rgdal)
 library(rgeos)
 library(readr)
 library(tidyverse)
+library(Matrix)
 
 #functions
 SPATIALIZEcafos <- function(df, crs) { 
@@ -21,11 +22,46 @@ SPATIALIZEcensus <- function(df, crs) {
     st_centroid()
 }
 
-
 CREATEbuffers <- function(df, distance) { 
   # distance: in miles
   st_buffer(df, units::set_units(distance, miles))
-}   
+} 
+
+OUTPUTresults <- function(d, d_inner, centroids, cafos, statename) { 
+  
+  buffers_inner <- CREATEbuffers(centroids, d_inner)
+  buffers_outer <- CREATEbuffers(centroids, d) 
+  
+  sf.pts <- st_sf(geometry = centroids)
+  
+  if (d_inner == 0) {
+  mat.df = st_intersects(buffers_outer, cafos_spatialized, sparse = TRUE)
+  } else {
+  sep_rings = st_sfc(map2(buffers_outer$geometry, buffers_inner$geometry, ~ st_difference(.x, .y)), crs = 102003)
+  sf_opt1 <- sf.pts %>%
+    st_set_geometry(sep_rings)
+  mat.df = st_intersects(sf_opt1, cafos, sparse = TRUE)
+  }
+  
+  
+  # -- convert results to data.frame (via sparse matrix)
+  n.ids <- sapply(mat.df, length)
+  vals <- unlist(mat.df)
+  
+  # -- catch when returns no values 
+  if (length(vals) != 0) {
+    out <- sparseMatrix(vals, rep(seq_along(n.ids), n.ids))
+    out.summ <- summary(out)
+    out.df <- data.frame(census_tract = cents[out.summ$j,]$TRACT, cafo_fid = cafos_spatialized[out.summ$i,]$cafo_fid)
+    d.rev <- sub("[.]", "p", d)
+    out.file <- paste0("CensusTracts_", statename, "_CAFO_BufferMatches_", d_inner, "_", d.rev,  ".csv")
+    write.csv(out.df, file = out.file)
+  } else {
+    print(paste0("NO CAFOS IN THIS ZONE"))
+  }
+  print(paste0("ALL DONE")) 
+}
+
 
 #read in census track shape file
 shape <- st_read("gz_2010_19_140_00_500k.shp")
@@ -33,52 +69,46 @@ shape <- st_read("gz_2010_19_140_00_500k.shp")
 #read in CAFOS csv
 cafos <- read.csv("IA_Merge_CAFOs_Coords.csv", stringsAsFactors = FALSE)
 cafos2 <- na.omit(cafos)
-cafos2$ID <- parse_number(cafos2$cafo_fid)
 
 #spatialize census track shapefile and CAFOs to correct grid
 cafos_spatialized <- SPATIALIZEcafos(cafos2, 102003)
 cents <- SPATIALIZEcensus(shape, 102003)
 
-#create buffers
-buffers_0.01 <- CREATEbuffers(cents, 0.01)
-buffers_0.05 <- CREATEbuffers(cents, 0.05)
-buffers_0.1 <- CREATEbuffers(cents, 0.1)
-buffers_0.5 <- CREATEbuffers(cents, 0.5)
-buffers_1 <- CREATEbuffers(cents, 1)
 
-#find intersections
-mat.df = st_intersects(buffers_0.01, cafos_spatialized, sparse = TRUE)
-mat.df2 = st_intersects(buffers_0.05, cafos_spatialized, sparse = TRUE)
-mat.df3 = st_intersects(buffers_0.1, cafos_spatialized, sparse = TRUE)
-mat.df4 = st_intersects(buffers_0.5, cafos_spatialized, sparse = TRUE)
-mat.df5 = st_intersects(buffers_1, cafos_spatialized, sparse = TRUE)
+#buffer of 0.05 
+OUTPUTresults(0.05, 0, cents, cafos_spatialized, statename="IA")
 
-#get values 
-n.ids <- sapply(mat.df, length)
-vals <- unlist(mat.df)
+#buffer of 0.25
+OUTPUTresults(0.25, 0.05, cents, cafos_spatialized, statename="IA")
 
-n.ids2 <- sapply(mat.df2, length)
-vals2 <- unlist(mat.df2)
+#buffer of 0.5 
+OUTPUTresults(0.5, 0.25, cents, cafos_spatialized, statename="IA")
 
-n.ids3 <- sapply(mat.df3, length)
-vals3 <- unlist(mat.df3)
 
-n.ids4 <- sapply(mat.df4, length)
-vals4 <- unlist(mat.df4)
+#buffer of 0.75
+OUTPUTresults(0.75, 0.5, cents, cafos_spatialized, statename="IA")
 
-n.ids5 <- sapply(mat.df5, length)
-vals5 <- unlist(mat.df5)
+#buffer of 1.0 
+OUTPUTresults(1, 0.75, cents, cafos_spatialized, statename="IA")
 
-#match values to original CAFO id and geometry 
-within_0.01 <- as.data.frame(n.ids)
-within_0.05 <- as.data.frame(n.ids2)
-within_0.1 <- as.data.frame(n.ids3)
-within_0.5 <- as.data.frame(n.ids4)
-within_1 <- as.data.frame(n.ids5)
+#buffer of 1.5
+OUTPUTresults(1.5, 1, cents, cafos_spatialized, statename="IA")
 
-#THIS ONLY GIVES THE NUMBERS OF CAFOS NOT THE CAFOS VALUES THEMSELVES
-shape$within_0.01 <- within_0.01$n.ids
-shape$within_0.05 <- within_0.05$n.ids
-shape$within_0.1 <- within_0.1$n.ids
-shape$within_0.5 <- within_0.5$n.ids
-shape$within_1 <- within_1$n.ids
+#buffer of 2.0
+OUTPUTresults(2, 1.5, cents, cafos_spatialized, statename="IA")
+
+#buffer of 2.5
+OUTPUTresults(2.5, 2, cents, cafos_spatialized, statename="IA")
+
+#buffer of 3.0
+OUTPUTresults(3, 2.5, cents, cafos_spatialized, statename="IA")
+
+#buffer of 3.5
+OUTPUTresults(3.5, 3, cents, cafos_spatialized, statename="IA")
+
+#buffer of 4.0
+OUTPUTresults(4, 3.5, cents, cafos_spatialized, statename="IA")
+
+#buffer of 4.5
+OUTPUTresults(4.5, 4, cents, cafos_spatialized, statename="IA")
+
